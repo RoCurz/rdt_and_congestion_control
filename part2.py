1dc9ff0ce6602be550ef06dc5a032de62fb528c9
RockersElite <cs1210121@iitd.ac.in>
2023-10-25 17:41:36 +0530

    Added some print statements

--- part2.py
+++ part2.py
@@ -4,6 +4,7 @@ time_out=100
 def receive_data(sock):
     global offset_data
     global offset_received
+    global Squished
     while True:
         try:
             sock.settimeout(.000000001)
@@ -16,29 +17,32 @@ def receive_data(sock):
             for substring in response:
                 if add_to_string:
                     string += substring+'\n'
+                if substring == "Squished":
+                    Squished = 1
                 if not add_to_string and substring == '':
                     add_to_string = True
             offset_data[(offset)//1448] = string[:-1]
-            time_run = (time.time()-start_time)*1000
-            # receive_time[offset] = time_run
-            receive_time.write(f"{offset}\t{time_run:.2f}\n")
+            # time_in_program = (time.time()-start_time)*1000
+            # receive_time.write(f"{offset}\t{time_in_program:.2f}\n")
             # print(f"{offset//1448} Received")
+            time_run = (time.time()-msg_time)*1000
             if (time_run>=time_out):
                 break
         except socket.timeout:
-            time_run = (time.time()-start_time)*1000
+            time_run = (time.time()-msg_time)*1000
             if (time_run>=time_out):
                 break
             continue
 
 # Server information
-server_ip = socket.gethostbyname("vayu.iitd.ac.in")
-server_port = 9802
+server_ip =  "10.17.7.134"#socket.gethostbyname("vayu.iitd.ac.in")
+server_port = 9801
 
 # Create a UDP socket
 udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 size_to_receive = 0
 
+start_time = time.time()
 while (size_to_receive==0):
     # Send the "SendSize" command to the server
     udp_socket.sendto("SendSize\nReset\n\n".encode('utf-8'), (server_ip, server_port))
@@ -61,16 +65,17 @@ no_of_request = (size_to_receive+1448-1)//1448
 offset_received = [False]*no_of_request
 offset_data = [""]*no_of_request
 print("no of request: ",no_of_request)
-receive_time = open("receive.txt","w")
-request_time = open("request.txt","w")
+# receive_time = open("receive.txt","w")
+# request_time = open("request.txt","w")
 
 first_False = 0
-time.sleep(.05)
+time.sleep(.0005)
 burst_size=1
 threshold=math.inf
 min_burst=1
 while (first_False < no_of_request):
-    print("burst_size",burst_size)
+    # print(f"burst_size:\t{burst_size}")
+    # print(f"threshold:\t{threshold}")
     k = 0
     frontier = []
     while (len(frontier)!=burst_size and ((first_False+k)<no_of_request)):
@@ -80,7 +85,7 @@ while (first_False < no_of_request):
     
     if len(frontier)==0:
         break
-    start_time = time.time()
+    msg_time = time.time()
     for offset_number in frontier:
         if offset_number == no_of_request-1:
             numbytes = size_to_receive%1448
@@ -89,13 +94,12 @@ while (first_False < no_of_request):
         else:
             numbytes = 1448
         udp_socket.sendto(f"Offset: {offset_number*1448}\nNumBytes: {numbytes}\n\n".encode('utf-8'), (server_ip, server_port))
-        time_run = (time.time()-start_time)*1000
-        # if offset_number*1448 in request_time:
-        #     request_time[offset_number*1448].append(str(time_run))
-        # else:
-        #     request_time[offset_number*1448] = [str(time_run)]
-        request_time.write(f"{offset_number*1448}\t{time_run:.2f}\n")
+        # time_in_program = (time.time()-start_time)*1000
+        # request_time.write(f"{offset_number*1448}\t{time_in_program:.2f}\n")
+    Squished = 0
     receive_data(udp_socket)
+    time_in_program = (time.time()-start_time)*1000
+    print(f"{time_in_program:.2f}\t{burst_size}\t{Squished}")
     has_false = False
     for offset_number in frontier:
         if offset_received[offset_number] == False:
@@ -103,7 +107,7 @@ while (first_False < no_of_request):
             has_false = True
             threshold=max(int(burst_size/2),1)
             burst_size=min_burst
-            print("dropped")
+            # print("dropped")
             break
     if not has_false:
         first_False = frontier[-1] + 1
